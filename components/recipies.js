407009c5b35f651ed5f438edb0d14eaceae561c8
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-03-21 10:21:53 +0530

    :sparkles: navbar

---
+++ components/recipies.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Recipe = () => {
+  return (
+    <div className="flex ">
+      <Link href="">
+        <div className="flex items-center justify-center w-64 h-64 duration-500 bg-[#07C731] rounded-lg hover:items-start hover:pt-10 hover:h-96 hover:w-72">
+          <div className="relative inline-block transition duration-500 ease-in-out group delay-10 hover:-translate-y-3 hover:scale-110">
+            <div className="h-60 w-60">
+              <img
+                src="https://veggiedesserts.com/wp-content/uploads/2021/04/paneer-butter-masala-sq.jpg"
+                className="w-full h-full"
+                alt=""
+              />
+            </div>
+
+            <div className="absolute hidden mt-4 font-semibold text-white text-m group-hover:block px-14">
+              <span className="">Main Auditorium</span>
+              <p className="mx-2 mt-2">Capacity: 3000</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+export default Recipe;
